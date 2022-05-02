5416b222319b0ce8a759326f56c9fa2145839ec0
alexArdigo <alexaardigo@gmail.com>
2022-05-02 18:44:13 +0100

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const userInput = document.getElementById("UserInput");
+const submit = document.getElementById("Submit");
+const clearList = document.getElementById("ClearList");
+
+document.getElementById("UserInput").placeholder = "Add your item here";
+
+console.log(userInput)
