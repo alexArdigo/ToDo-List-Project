975daf0d7e4ac27e32a0ddd84e100e608d30e4d4
alexArdigo <alexaardigo@gmail.com>
2022-05-03 00:16:29 +0100

    commit

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const userInput = document.getElementById("UserInput");
 const submit = document.getElementById("Submit");
 const clearList = document.getElementById("ClearList");
 
-document.getElementById("UserInput").placeholder = "Add your item here";
+//const placeHolder = document.getElementById("UserInput").placeholder = 'Add your item here'
 
-console.log(userInput)
+const handleSubmit = () => {
+    const OL = document.createElement('OL');
+    OL.setAttribute('id', 'ToDoList');
+    document.body.appendChild(OL);
+
+    const LI = document.createElement('LI');
+    const userInputSubmited = document.createTextNode(userInput.value);
+    LI.appendChild(userInputSubmited)
+    document.getElementById('ToDoList').appendChild(LI);
+
+    document.getElementById("UserInput").placeholder = 'Add your item here'
+
+
+}
+
+const handleClearList = () => {
+    const element = document.getElementById('OL')
+    element.remove()
+}
+
+submit.addEventListener('click', handleSubmit)
+clearList.addEventListener('click', handleClearList)
