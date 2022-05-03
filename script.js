87ab4605f8489f3ae3cdcc8fbf2871486819ddc2
alexArdigo <alexaardigo@gmail.com>
2022-05-03 16:56:43 +0100

    commit

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const submit = document.getElementById("Submit");
 const clearList = document.getElementById("ClearList");
 
 
-//const DIV = document.createElement('div')
-
-
-
+// handleSubmit() checks if input field has been written, if so, 
+// creates a new OL with id(ToDoList) and appends to div#container. Next, 
+// creates a new LI with class(ListItems), adds userInput.value to it 
+// and appends to OL.
 const handleSubmit = () => {
     if (userInput.value.trim() === '') return
     const divContainer = document.getElementById('container');
     const OL = document.createElement('OL');
     OL.setAttribute('id', 'ToDoList');
-    divContainer.appendChild(OL)
+    divContainer.appendChild(OL);
     const LI = document.createElement('LI');
     LI.setAttribute('class', 'ListItems')
     const userInputSubmited = document.createTextNode(userInput.value);
@@ -24,6 +24,8 @@ const handleSubmit = () => {
 
 }
 
+// handleClearList() triggers a confirm message to weither 
+// clear div#container's content or not. The action is unreversible.
 const handleClearList = () => {
     const divContainer = document.getElementById('container');
     const message = 'Are you sure you want to delete the whole list?'
@@ -31,6 +33,13 @@ const handleClearList = () => {
         const element = document.querySelector('#ToDoList');
         divContainer.removeChild(element);
     }
+}
+
+const handleItem = () => {
+    const getsLI = document.querySelector('.ListItems');
+    const deleteItemButton = document.createElement("button");
+    deleteItemButton.innerHTML('X');
+     
 }
 
 submit.addEventListener('click', handleSubmit);
