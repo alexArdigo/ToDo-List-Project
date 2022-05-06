8af698595635c72e26bacb0b799ea2a64149a1ac
alexArdigo <alexaardigo@gmail.com>
2022-05-06 15:59:16 +0100

    commit

--- script.js
+++ script.js
@@ -4,17 +4,29 @@ const userInput = document.getElementById('User-Input');
 userInput.addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
         event.preventDefault();
-        document.getElementById('Submit-Input').click();
+        document.getElementById('Submit-Input');
+
     }
 })
 
 // Submits event
 const submitUserInput = (event) => {
     event.preventDefault();
-    console.log("hello");
+    appendLi()
+    userInput.value = '';
 }
 const formID = document.getElementById('form-control');
 formID.addEventListener('submit', submitUserInput)
+
+// Appends <li> to <ol> 
+const appendLi = () => {
+    const LI = document.createElement('li');
+    LI.setAttribute('id', `item${liID}`);
+    LI.appendChild(document.createTextNode(userInput.value))
+    OL.append(LI);
+    liID += 1
+    
+}
 
 // Clears list
 const clearList = (event) => {
@@ -23,5 +35,3 @@ const clearList = (event) => {
 }
 const clearListID = document.getElementById('Clear-List');
 clearListID.addEventListener('click', clearList)
-
-// Creates <li>
--- script_DOM.js
+++ script_DOM.js
@@ -1,8 +1,9 @@
-const BODY = document.body
+const BODY = document.body;
 
 // DIV
 const DIV1 = document.createElement('div');
-DIV1.setAttribute('class', 'To-Do-List')
+DIV1.setAttribute('class', 'To-Do-List');
+DIV1.setAttribute('id', 'DIV1');
 BODY.append(DIV1);
 
 // H1
@@ -35,10 +36,19 @@ FORM.append(SUBMIT);
 // CLEAR LIST
 const CLEARLIST = document.createElement('button');
 CLEARLIST.setAttribute('id', 'Clear-List');
-CLEARLIST.innerText = 'Clear List'
+CLEARLIST.innerText = 'Clear List';
 FORM.append(CLEARLIST);
 
-// 
+// DIV2
 const DIV2 = document.createElement('div');
-DIV2.setAttribute('class', 'To-Do-List')
+DIV2.setAttribute('class', 'To-Do-List');
+DIV2.setAttribute('id', 'DIV2');
 BODY.append(DIV2);
+
+// OL
+const OL = document.createElement('ol');
+OL.setAttribute('id', 'Ordered-List');
+DIV2.append(OL)
+
+// LI
+let liID = 1;
