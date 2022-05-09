dad879b419a811e71cab8ae824aeceb674598d0e
alexArdigo <alexaardigo@gmail.com>
2022-05-09 18:08:17 +0100

    commit

--- script.js
+++ script.js
@@ -22,10 +22,41 @@ formID.addEventListener('submit', submitUserInput)
 const appendLi = () => {
     const LI = document.createElement('li');
     LI.setAttribute('id', `item${liID}`);
-    LI.appendChild(document.createTextNode(userInput.value))
+    LI.appendChild(document.createTextNode(userInput.value + ' '));
+    LI.append(doneBTN());
+    LI.append(addXBTN());
     OL.append(LI);
     liID += 1
 }
+
+// Adds Done <button> to <li>
+const doneBTN = () => {
+    const liDoneBTN = document.createElement('button');
+    liDoneBTN.innerHTML = 'Done!';
+    return liDoneBTN
+}
+
+// Adds delete (X) <button> to <li>
+const addXBTN = () => {
+    const liXBTN = document.createElement('button');
+    liXBTN.setAttribute('id', `item${liBtnID}`);
+    liXBTN.setAttribute('onClick', 'deleteItem(this.id)')
+    liXBTN.innerText = 'X';
+    return liXBTN
+}
+
+// TRY TO MAKE IT WORK WITH TARGET.VALUE
+const deleteItem = (item) => {
+    
+    const itemToDelete = document.getElementById(item);
+    OL.remove(itemToDelete)
+}
+
+//const deleteItemBTN = document.getElementById();
+//deleteItemBTN.addEventListener('click', () => deleteItem());
+
+
+
 
 // Clears list
 const clearList = (event) => {
@@ -39,18 +70,3 @@ const clearList = (event) => {
 }
 const clearListBTN = document.querySelector('#Clear-List');
 clearListBTN.addEventListener('click', clearList);
-
-
-// Adds delete (X) <button> to <li>
-const addXBTN = () => {
-    const liXBTN = document.createElement('button');
-    liXBTN.setAttribute('id', `item${liBtnID}`);
-    liXBTN.innerText = 'X';
-    OL.append(liXBTN);
-}
-
-const deleteItem = () => {
-
-}
-const deleteItemBTN = document.querySelector('button');
-deleteItemBTN.addEventListener('click', deleteItem);
